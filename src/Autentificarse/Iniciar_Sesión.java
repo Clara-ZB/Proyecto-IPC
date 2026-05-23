30053863dff7ece643ce1acf6c36f91cbc280f07
marta <marta@Portatilmmm.mshome.net>
2026-05-23 18:52:47 +0200

    Merge origin/master
    # Conflicts:
    #	nbproject/build-impl.xml
    #	nbproject/genfiles.properties
    #	nbproject/project.properties
    #	nbproject/project.xml
    #	src/mapademo/FXMLDocumentController.java

---
+++ src/Autentificarse/Iniciar_Sesión.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Autentificarse;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author marta
+ */
+public class Iniciar_Sesión implements Initializable {
+
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private TextField txtContraseña;
+    @FXML
+    private Text errorUsuario;
+    @FXML
+    private Text errorContraseña;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnIS;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleIS(ActionEvent event) {
+    }
+    
+}
